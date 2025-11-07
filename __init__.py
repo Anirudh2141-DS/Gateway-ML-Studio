69e2db8c4d69c5e208979bd018c76c28bae0d1ca
Anirudh Reddy Ninganpally <94893515+Anirudh2141-DS@users.noreply.github.com>
2025-11-07 11:20:23 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,33 +1,8 @@
-# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2015 Eric Larson
 #
-# Copyright (C) 2012-2023 Vinay Sajip.
-# Licensed to the Python Software Foundation under a contributor agreement.
-# See LICENSE.txt and CONTRIBUTORS.txt.
-#
-import logging
+# SPDX-License-Identifier: Apache-2.0
 
-__version__ = '0.3.9'
+from pip._vendor.cachecontrol.caches.file_cache import FileCache, SeparateBodyFileCache
+from pip._vendor.cachecontrol.caches.redis_cache import RedisCache
 
-
-class DistlibException(Exception):
-    pass
-
-
-try:
-    from logging import NullHandler
-except ImportError:  # pragma: no cover
-
-    class NullHandler(logging.Handler):
-
-        def handle(self, record):
-            pass
-
-        def emit(self, record):
-            pass
-
-        def createLock(self):
-            self.lock = None
-
-
-logger = logging.getLogger(__name__)
-logger.addHandler(NullHandler())
+__all__ = ["FileCache", "SeparateBodyFileCache", "RedisCache"]
